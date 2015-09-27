cb7f6deda06de03c1fef554ac58439fbf920091a
Johannes Wienke <languitar@semipol.de>
2015-09-27 16:53:50 +0200

    Prepare version for next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='i3-workspace-switch',
-    version='0.1',
+    version='0.2-dev',
 
     author='Johannes Wienke',
     author_email='languitar@semipol.de',
